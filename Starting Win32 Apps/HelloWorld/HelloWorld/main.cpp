16032855817f297d87fcfad1a37277f3c7a093e1
jacknick7 <josep.ciurana@est.fib.upc.edu>
2022-03-18 21:54:53 +0100

    added my first DirectX app, CircleDirect2D

--- Starting Win32 Apps/HelloWorld/HelloWorld/main.cpp
+++ Starting Win32 Apps/HelloWorld/HelloWorld/main.cpp
@@ -6,8 +6,9 @@
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
+
     // Register the window class.
     const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
@@ -44,7 +45,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     ShowWindow(hwnd, nCmdShow);
 
     // Run the message loop.
-
     MSG msg = { };
     while (GetMessage(&msg, NULL, 0, 0))
     {
@@ -68,14 +68,13 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
-
-
+        // All painting occurs here, between BeginPaint and EndPaint.
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
         EndPaint(hwnd, &ps);
     }
     return 0;
-
     }
+
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
+
